0f0c17d6da7ca5fce17159f410e9ef824bd910c1
SycaTech <148126462+SycaTech@users.noreply.github.com>
2026-01-30 12:24:01 +0200

    intake
    thtryt

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Intake.java
@@ -3,38 +3,39 @@ package org.firstinspires.ftc.teamcode.Subsystems;
 import com.arcrobotics.ftclib.command.Command;
 import com.arcrobotics.ftclib.command.InstantCommand;
 import com.arcrobotics.ftclib.command.SubsystemBase;
+import com.arcrobotics.ftclib.hardware.motors.Motor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.navigation.Velocity;
 
 public class Intake extends SubsystemBase {
     private Telemetry telemetry;
-    public static Intake INSTANCE = new Intake();
+    private HardwareMap hardwareMap;
 
-    public Intake() {
-
+    public Intake(Telemetry telemetry, HardwareMap hwMap) {
+        this.telemetry = telemetry;
+        this.hardwareMap = hwMap;
+        intake = hwMap.get(Motor.class, "intake");
     }
 
-    public DcMotorEx intake;
-
-    public void init(HardwareMap hwMap) {
-        intake = hwMap.get(DcMotorEx.class, "intake");
-    }
+    public Motor intake;
 
     final double TICKS_PER_REVOLUTION = 103.6;
 
-    public void periodic(Telemetry telemetry) {
-        double velocityTPSL = intake.getVelocity();
+    @Override
+    public void periodic() {
+        double velocityTPSL = intake.getRate();
         double velocityRPML = (velocityTPSL / TICKS_PER_REVOLUTION) * 60.0;
 
-        telemetry.addData("power", intake.getPower());
+        telemetry.addData("power", intake.get());
         telemetry.update();
 
     }
 
     public void setPower(double power) {
-        intake.setPower(power);
+        intake.set(power);
     }
 
     public Command Power(double VOLT) {
